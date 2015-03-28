b54f5f310aa1e99a31a936d224a92a3d1618dd20
saransasidharan <saran.s.dhar@gmail.com>
2015-03-28 16:02:21 +0530

    CG and DG with Rusinov flux

--- dg1d.py
+++ dg1d.py
@@ -76,6 +76,15 @@ def element_diff_matrix(element_order, weight, zeta):
     return diff
 
 
+def rusinov_1d(element_order):
+    row = element_order+1
+    col = row+1
+    rusin = np.zeros((row, col))
+    rusin[0, 0] = -1.0
+    rusin[-1, -1] = +1.0
+    return rusin
+
+
 def mass_stiff(n_element, element_mat):
     n, _ = np.shape(element_mat)
     N = ((n-1)*n_element)+1
@@ -91,6 +100,7 @@ def mass_stiff(n_element, element_mat):
     stif_mat[k:k+n-1, N-1] = 0
     return stif_mat
 
+
 def diff_stiff(n_element, element_mat):
     n, _ = np.shape(element_mat)
     N = ((n-1)*n_element)+1
@@ -98,6 +108,30 @@ def diff_stiff(n_element, element_mat):
     diff_mat[N-1, N-1] = 0
     diff_mat[0, 0] = 0
     return diff_mat
+
+
+def mass_stiff_disc(n_element, element_mat):
+    n, _ = np.shape(element_mat)
+    N = n*n_element
+    stif_mat = np.zeros(( N, N))
+    for i in range(n_element):
+        k = i*n
+        stif_mat[k:k+n, k:k+n] = element_mat
+    return stif_mat
+
+
+def diff_stiff_disc(n_element, element_mat):
+    n, _ = np.shape(element_mat)
+    rus = rusinov_1d(n-1)
+    N = n*n_element
+    diff_mat = mass_stiff_disc(n_element, element_mat)
+    for i in range(1, n_element):
+        k = i*n
+        diff_mat[k:k+n, k-1:k+n] -= rus
+    diff_mat[0, -1] -= rus[0, 0]
+    diff_mat[n-1, n-1] -= rus[-1, -1]
+    return diff_mat
+
 
 def r_matrix(msm, dsm, u):
     R_mat = np.dot(np.linalg.inv(msm), dsm)*-1*u
@@ -117,6 +151,18 @@ def x_domain(element_order, n_element, x_min=-1.0, x_max=+1.0):
     return x
 
 
+def x_domain_disc(element_order, n_element, x_min=-1.0, x_max=+1.0):
+    N = (element_order+1)*n_element
+    x = np.zeros(N)
+    Dx = abs(x_max-x_min)/n_element
+    zeta = lb.root(element_order+1)
+    for i in range(n_element):
+        k = i*(element_order+1)
+        x[k:k+element_order+1] = x_min + (zeta+1.0)*(Dx/2.0)
+        x_min = x_min+Dx
+    return x
+
+
 def dt_calc(courant, u, dx):
     return (courant*dx)/u
 
@@ -125,7 +171,6 @@ def rk_2nd(r_mat, initial, T, dt):
     steps = np.round(T/dt)
     steps.astype(int)
     q = initial
-    q_half, q_temp = q*0, q*0
     for i in np.arange(steps):
         q_half = q + np.dot(r_mat, q)*(dt/2)
         q_temp = q + np.dot(r_mat, q_half)*dt
@@ -133,31 +178,45 @@ def rk_2nd(r_mat, initial, T, dt):
     return q
 
 
+def cont_galerkin(T, n_lobatto, element_order, n_element, u=2.0, courant=1.0/4.0, sigma=1.0/8.0):
+    X = x_domain(element_order, n_element)
+    initial = np.e**(-1*(X/(2*sigma))**2)
+    Dx = np.min(abs(X[1:element_order+1]-X[0:element_order]))
+    zeta = lb.root(n_lobatto)
+    weight = ig.lobatto_weight(n_lobatto)
+    edm = element_diff_matrix(element_order, weight, zeta)
+    emm = element_mass_matrix(element_order, weight, zeta)*(2.0/n_element)
+    dsm = diff_stiff(n_element, edm)
+    msm = mass_stiff(n_element, emm)
+    R_mat = r_matrix(msm, dsm, u)
+    dt = dt_calc(courant, u, Dx)
+    q = rk_2nd(R_mat, initial, T, dt)
+    return q, initial, X
+
+
+def disc_galerkin(T, n_lobatto, element_order, n_element, u=2.0, courant=1.0/4.0, sigma=1.0/8.0):
+    X = x_domain_disc(element_order, n_element)
+    initial = np.e**(-1*(X/(2*sigma))**2)
+    Dx = np.min(abs(X[1:element_order+1]-X[0:element_order]))
+    zeta = lb.root(n_lobatto)
+    weight = ig.lobatto_weight(n_lobatto)
+    edm = element_diff_matrix(element_order, weight, zeta)
+    emm = element_mass_matrix(element_order, weight, zeta)*(2.0/n_element)
+    edmD = np.transpose(edm)
+    dsmD = diff_stiff_disc(n_element, edmD)
+    msmD = mass_stiff_disc(n_element, emm)
+    R_mat = r_matrix(msmD, dsmD, u)*-1
+    dt = dt_calc(courant, u, Dx)
+    q = rk_2nd(R_mat, initial, T, dt)
+    return q, initial, X
+
+T = 1.0
 n_lobatto = 9
 element_order = 8
-n_element = 8
-u = 2.0
-courant = 1.0/4.0
-T = 1.0
-
-sigma = 1.0/8.0
-X = x_domain(element_order, n_element)
-initial = np.e**(-1*(X/(2*sigma))**2)
-Dx = np.min(X[1:]-X[:-1])
-
-
-zeta = lb.root(n_lobatto)
-weight = ig.lobatto_weight(n_lobatto)
-
-edm = element_diff_matrix(element_order, weight, zeta)
-emm = element_mass_matrix(element_order, weight, zeta)*(2.0/n_element)
-dsm = diff_stiff(n_element, edm)
-msm = mass_stiff(n_element, emm)
-
-R_mat = r_matrix(msm, dsm, u)
-dt = dt_calc(courant, u, Dx)
-
-q = rk_2nd(R_mat, initial, T, dt)
+n_element = 16
+#q, initial, X = disc_galerkin(T, n_lobatto, element_order, n_element)
+q, initial, X = cont_galerkin(T, n_lobatto, element_order, n_element)
+plt.plot(X, initial)
 plt.plot(X, q)
 plt.show()
 
